ac47c74de4f78a965ee6699473b45d7aebf16f9d
caiogsetec <143644880+caiogsetec@users.noreply.github.com>
2025-10-08 21:36:47 -0300

    Update TaskHistoryRepository.cs

--- Priorix/Data/Repositories/TaskHistoryRepository.cs
+++ Priorix/Data/Repositories/TaskHistoryRepository.cs
@@ -1,1 +1,34 @@
+using Priorix.Core.Interfaces.Repositories;
+using Priorix.Priorix.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Priorix.Data.Repositories
+{
+    public class TaskHistoryRepository : ITaskHistoryRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TaskHistoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TaskHistory history)
+        {
+            _context.TaskHistories.Add(history);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<TaskHistory> GetByTaskId(int taskId)
+        {
+            return _context.TaskHistories
+                           .Where(h => h.TaskId == taskId)
+                           .OrderByDescending(h => h.ChangeDate)
+                           .ToList();
+        }
+    }
+}
+
 
