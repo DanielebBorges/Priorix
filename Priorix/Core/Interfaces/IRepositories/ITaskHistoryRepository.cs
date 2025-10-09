6c9cd15fb041ac0db7460a437a108b6012a82090
caiogsetec <143644880+caiogsetec@users.noreply.github.com>
2025-10-08 21:35:47 -0300

    Update ITaskHistoryRepository.cs

--- Priorix/Core/Interfaces/IRepositories/ITaskHistoryRepository.cs
+++ Priorix/Core/Interfaces/IRepositories/ITaskHistoryRepository.cs
@@ -1,1 +1,11 @@
+using global::Priorix.Priorix.Core.Entities;
+namespace Priorix.Core.Interfaces.Repositories
+{
+    public interface ITaskHistoryRepository
+    {
+        void Add(TaskHistory history);
+        IEnumerable<TaskHistory> GetByTaskId(int taskId);
+    }
+}
+
 
