96c84911c902a47cbd12c906fc7db75ca75be643
caiogsetec <143644880+caiogsetec@users.noreply.github.com>
2025-10-10 09:34:41 -0300

    Update TaskHistoryController.cs

--- Priorix/API/Controllers/TaskHistoryController.cs
+++ Priorix/API/Controllers/TaskHistoryController.cs
@@ -1,1 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Priorix.Core.Services;
+using Priorix.Core.Interfaces;
+
+namespace Priorix.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskHistoryController : ControllerBase
+    {
+        private readonly TaskHistoryService _historyService;
+
+        public TaskHistoryController(ITaskHistoryRepository historyRepository)
+        {
+            _historyService = new TaskHistoryService(historyRepository);
+        }
+
+        [HttpGet("{taskId}")]
+        public IActionResult GetHistory(int taskId)
+        {
+            var history = _historyService.GetHistory(taskId);
+            return Ok(history);
+        }
+    }
+}
 
