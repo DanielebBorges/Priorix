ca5f0c3dda659c929cc3475589fb3b10f2a4b5cf
barbsrebechi <119419470+barbsrebechi@users.noreply.github.com>
2025-10-12 17:15:26 -0300

    Update ITaskService.cs

--- Priorix/Core/Interfaces/IServices/ITaskService.cs
+++ Priorix/Core/Interfaces/IServices/ITaskService.cs
@@ -1,10 +1,15 @@
-﻿namespace Priorix.Priorix.Core.Interfaces.IServices
+﻿using Priorix.Priorix.Core.Entities;
+
+namespace Priorix.Priorix.Core.Interfaces.IServices
 {
     public interface ITaskService
     {
-          TaskOutputDTO Cadastrar(TaskInputDTO dto);
-          TaskOutputDTO Priority(int id, string metodo); 
-          IEnumerable<TaskOutputDTO> Listar();
-          RelatorioDTO GerarRelatorio()
+        public List<Task> GetAllTasks();
+        public Task GetTaskById(int id);
+        public void CreateTask(Task task);
+        public void UpdateTask(Task task);
+        public void DeleteTask(int id);
+        public bool TaskExists(int id);
     }
 }
+
