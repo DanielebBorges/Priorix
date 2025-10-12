e3149c64bbacf17e822efb4308c68b6acccda917
barbsrebechi <119419470+barbsrebechi@users.noreply.github.com>
2025-10-12 17:07:53 -0300

    Update Task.cs

--- Priorix/Core/Entities/Task.cs
+++ Priorix/Core/Entities/Task.cs
@@ -1,22 +1,13 @@
-﻿namespace Priorix.Priorix.Core.Entities
+namespace Priorix.Priorix.Core.Entities
 {
     public class Task
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int ResponsibleUserId { get; set; }
-        public int StatusId { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime Deadline { get; set; }
-        public int EffortEstimated { get; set; }
-        public int ImpactExpected { get; set; }
-        public int Urgency { get; set; }
-        public int PriorityScore { get; set; }
-        public string AIRecommendation { get; set; }
-        public string Justification { get; set; }
-        public DateTime LastUpdatedAt { get; set; }
+        public DateTime DueDate { get; set; } // Data de vencimento
+        public bool IsCompleted { get; set; }
+        public int Priority { get; set; }
+        public int UserId { get; set; }
     }
-
-
 }
